9b260809942be02bb958e61bea597d9dfa2ade48
Emanuele <em.zaccaria@gmail.com>
2020-03-24 16:43:22 +0100

    js cognomi parte 1

---
+++ myscript.js
@@ -0,0 +1,37 @@
+// Chiedi all’utente il cognome, inseriscilo in un array con altri cognomi e stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione della lista in cui il nuovo utente si trova
+
+// Creazione array con Cognomi
+var ListaCognomi;
+ListaCognomi = ["Zanetti";"Abate";"Longo"];
+
+console.log(ListaCognomi);
+
+// Chiedi all'untente il cognome
+var CognomeUtente;
+CognomeUtente = prompt("Inserisci il tuo Cognome");
+
+console.log(CognomeUtente);
+
+// Inserisci il cognome all'interno della lista
+ListaCognomi.push(CognomeUtente);
+
+console.log(ListaCognomi);
+
+// ordina la lista alfabeticamente
+ListaCognomi.sort();
+
+console.log(ListaCognomi);
+
+// all'interno della lista cerca il nome e la sua posizione
+var i;
+var PosizioneCognome;
+for (var i = 0; i < ListaCognomi.length; i++) {
+  if (ListaCognomi[i] == CognomeUtente) {
+    PosizioneCognome == i;
+  }
+}
+// stampa la lista
+document.getElementById('cognome').innerHTML = ListaCognomi;
+
+// stampa la posizione del nome
